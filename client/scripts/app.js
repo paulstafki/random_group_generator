a027393f9c48b3ca4cbe6c5681828a5d6a16209e
paulstafki <paulstafki@gmail.com>
2015-08-04 16:13:00 -0500

    Added good notes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,15 +4,17 @@ var peopleArray = [];
 function shuffleKids(array) {
     for(var j, x, i = array.length; i; j = parseInt(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x);
     return array;
-};
+}
+
 function appendallthebuttons() {
     for (var i = 2; i < 12; i++){
-        $('.moButtons').append('<button class=groupButton data-number="'+i+'">'+i+'</button>');
+        $('.moButtons').append('<button class=groupButton data-number="'+i+'">'+i+'</button>'); //assigns a data attribute to the html based on 'i' - which is the current iteration of the for loop
     }
     $('.moButtons').append('<button class="randomize">randomize</button>');
 }
+
 function splitTeams (array) {
-    //remove kids of groups
+    $('.groups').children().remove();
     for (var i = 1; i <= numGroups; i++) {
         $('.groups').append('<div class="teams' + i + '">Team: ' +(i)+ '</div>');
     }
@@ -26,19 +28,20 @@ function splitTeams (array) {
         }
     }
 }
+
 $(document).ready(function (){
+    appendallthebuttons();//appends buttons on page load
     $.ajax({
         url: "/data",
         success: function(data){
             console.log(data);
-            appendallthebuttons();//appends buttons on page load
             $.each(data, function() { //each loop to push names to the peepsArray
                 peopleArray.push(this.name);
             });
         }
     });
     $('body').on('click','.groupButton', function() {//add data attributes
-        numGroups = $(this).data("number");
+        numGroups = $(this).data("number");         //this assigns the number of the button to the numGroups variable using a data attribute in the html
         console.log(numGroups);
     });
     $('body').on('click','.randomize', function() {
